ff9f0b3b9aa54b8e1f7d76da2827d2872acb06bf
Hadson00 <hadson798@gmail.com>
2024-03-05 22:21:25 -0300

    AddEx

---
+++ ex1.py
@@ -0,0 +1,21 @@
+contador = 0
+vetor = []
+
+while contador < 30:
+    x = int(input("digite um numero positivo ou negativo para vetor: "))
+    vetor.append(x)
+    contador += 1
+
+contador = 0
+while contador < 30:
+    if vetor[contador] > 0: 
+        x = 1
+        print(x)
+        contador += 1
+    elif vetor[contador] < 0: 
+        x = 0
+        print(x)
+        contador += 1
+    else:
+        print(x)
+        contador += 1
